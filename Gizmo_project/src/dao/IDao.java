13b5acb4f16f168bd63584aa224f8bd5d3231cec
Sofia Ågren <sofiaagren1@hotmail.com>
2020-10-16 14:22:22 +0200

    bara tok

---
+++ Gizmo_project/src/dao/IDao.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.util.List;
+/**
+ * Interface for all CRUD operations of DAO classes
+ * @author awi
+ *
+ * @param <T> One of the intended model classes that a DAO
+ * implementation should be able to handle
+ */
+
+public interface IDao<T> {
+	
+	T get(int id);
+	List<T> getAll();
+	T save(T t);
+	void update(T t, String[] params);
+	void delete(T t);
+}
--- Gizmo_project/src/model/UserList.java
+++ Gizmo_project/src/model/UserList.java
@@ -1,18 +1,63 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
+import java.util.List;
 
-public class UserList extends LinkedList<User> {
+import dao.IDao;
+import db.DbConnectionManager;
+
+public class UserList extends LinkedList<User> implements IDao<User>{
 
 	private static final long serialVersionUID = 1L;
 	private static UserList instance;
+	static DbConnectionManager dbConManagerSingleton = null;
 	
 	private UserList() {}
 	
 	public static UserList getInstance() {
 		if(instance == null) {
 			instance = new UserList();
+			dbConManagerSingleton = DbConnectionManager.getInstance();
 		}
 		return instance;
+	}
+
+	@Override
+	public List<User> getAll() {
+		
+		
+		
+		try {
+			ResultSet resultSet = dbConManagerSingleton.excecuteQuery("SELECT id, name, birth_year FROM students");
+			while (resultSet.next()) {
+				UserList.getInstance().add(new User(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.get(4));
+				
+			}
+			dbConManagerSingleton.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return UserList.getInstance().getAll();
+	}
+
+	@Override
+	public User save(User t) {
+		
+		return ;
+	}
+
+	@Override
+	public void update(User t, String[] params) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User t) {
+		// TODO Auto-generated method stub
+		
 	}
 }
